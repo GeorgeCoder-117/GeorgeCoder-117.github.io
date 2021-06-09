ab6f88204e1e568801896b8715ea5ec81a91a3b9
GeorgeCoder-117 <85602139+GeorgeCoder-117@users.noreply.github.com>
2021-06-09 00:13:16 -0500

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+(function() {
+
+	window.onload = function() {
+
+		$(function(){
+    	    $("#skillcircle-1").percircle({progressBarColor: '#00CBCE'});
+    	    $("#skillcircle-2").percircle({progressBarColor: '#00CBCE'});
+    	    $("#skillcircle-3").percircle({progressBarColor: '#00CBCE'});
+    	    $("#skillcircle-4").percircle({progressBarColor: '#00CBCE'});
+    	});
+
+    }	
+
+})();
